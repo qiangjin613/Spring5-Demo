b28b7c9bde5fe89e441232c0a2bc4c23280d8170
qiangjin613 <80807530+qiangjin613@users.noreply.github.com>
2022-03-22 14:23:42 +0800

    Update User.java

--- src/com/spring/demo/bean/type/User.java
+++ src/com/spring/demo/bean/type/User.java
@@ -3,6 +3,9 @@ package com.spring.demo.bean.type;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * 普通 Bean
+ */
 public class User {
     public void add() {
         System.out.println("执行 User 的 add()");
@@ -10,7 +13,7 @@ public class User {
 }
 
 class Test {
-    public static void testAdd() {
+    public static void main(String[] args) {
         // 1. 加载 Spring 配置文件
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 
@@ -18,9 +21,5 @@ class Test {
         User user = context.getBean("user", User.class);
         user.add();
         System.out.println(user);
-    }
-
-    public static void main(String[] args) {
-        testAdd();
     }
 }
